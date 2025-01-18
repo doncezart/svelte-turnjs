85883973f1aa150d2e23792e616504bb90f57501
cezar <doncezart>
2025-01-18 21:54:19 +0000

    fix?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
     import { ImageFlip } from '$lib/index.js'
-    let images =[
-		"https://raw.githubusercontent.com/blasten/turn.js/refs/heads/master/demos/magazine/pages/01.jpg",
-		"https://raw.githubusercontent.com/blasten/turn.js/refs/heads/master/demos/magazine/pages/02.jpg",
-		"https://raw.githubusercontent.com/blasten/turn.js/refs/heads/master/demos/magazine/pages/03.jpg",
-		"https://raw.githubusercontent.com/blasten/turn.js/refs/heads/master/demos/magazine/pages/04.jpg"
-	]
-	let size = [400,600]
-</script>
-
-<ImageFlip {size} {images} />
+  
+    let images = [
+      'https://picsum.photos/400/600/',
+      'https://picsum.photos/400/600/',
+    ]
+    let size = [400, 600]
+  </script>
+  
+  <ImageFlip {size} {images} />
